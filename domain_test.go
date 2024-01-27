2f3018630a16ada51da63088303d8de0ba1c8bcd
Mike Kravtsov <m.kravtsov@fetchrewards.com>
2024-01-26 22:17:28 -0600

    test

--- domain_test.go
+++ domain_test.go
@@ -1,17 +1,104 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"testing"
 )
 
 func TestCalculatePoints(t *testing.T) {
-	receipt := Receipt{Total: "100.00"}
 
-	expectedPoints := 5
+	testJson1 :=
+		`
+	{
+		"retailer": "Target",
+		"purchaseDate": "2022-01-01",
+		"purchaseTime": "13:01",
+		"items": [
+		  {
+			"shortDescription": "Mountain Dew 12PK",
+			"price": "6.49"
+		  },{
+			"shortDescription": "Emils Cheese Pizza",
+			"price": "12.25"
+		  },{
+			"shortDescription": "Knorr Creamy Chicken",
+			"price": "1.26"
+		  },{
+			"shortDescription": "Doritos Nacho Cheese",
+			"price": "3.35"
+		  },{
+			"shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
+			"price": "12.00"
+		  }
+		],
+		"total": "35.35"
+	  }
+	`
 
-	receipt.calculatePoints()
-	
-	if receipt.Points != expectedPoints {
-		t.Errorf("calculatePoints() = %v; want %v", points, expectedPoints)
+	var receipt1 Receipt
+
+	receipt1Err := json.Unmarshal([]byte(testJson1), &receipt1)
+
+	if receipt1Err != nil {
+		log.Println(receipt1Err)
+		return
+	}
+
+	testJson2 :=
+		`
+		{
+			"retailer": "M&M Corner Market",
+			"purchaseDate": "2022-03-20",
+			"purchaseTime": "14:33",
+			"items": [
+			  {
+				"shortDescription": "Gatorade",
+				"price": "2.25"
+			  },{
+				"shortDescription": "Gatorade",
+				"price": "2.25"
+			  },{
+				"shortDescription": "Gatorade",
+				"price": "2.25"
+			  },{
+				"shortDescription": "Gatorade",
+				"price": "2.25"
+			  }
+			],
+			"total": "9.00"
+		  }
+	`
+
+	var receipt2 Receipt
+
+	receipt2Err := json.Unmarshal([]byte(testJson2), &receipt2)
+	if receipt2Err != nil {
+		log.Println(receipt2Err)
+		return
+	}
+
+	tests := []struct {
+		Receipt Receipt
+		ExpectedPoints int
+	}{
+		{
+			Receipt: receipt1,
+			ExpectedPoints: 28,			
+		},
+		{
+			Receipt: receipt2,
+			ExpectedPoints: 109,			
+		},
+		
+	}
+
+	for _, test := range tests {
+
+		test.Receipt.calculatePoints()
+
+		if test.Receipt.Points != test.ExpectedPoints {
+			t.Errorf("When receipt is: %+v, Expected %v points, but got %v points", test.Receipt, test.ExpectedPoints, test.Receipt.Points)
+		}
 	}
 }
